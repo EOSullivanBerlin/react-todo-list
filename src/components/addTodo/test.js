011852167f84b64b1fb14196d9b41a9679efa4b3
EOSullivanBerlin <esully666@gmail.com>
2018-01-28 16:18:20 +0100

    Passing Unit tests and Function tests

--- src/components/addTodo/test.js
+++ src/components/addTodo/test.js
@@ -3,28 +3,37 @@ import { shallow, mount } from 'enzyme';
 import AddTodo from '.';
 
 describe('AddTodo component', () => {
+  let component;
+  const submitMock = jest.fn();
+
+  beforeEach(() => {
+    component = shallow(
+      <AddTodo
+        submitTodo={submitMock}
+      />,
+    );
+  });
+
   it('Should render successfully', () => {
-    const component = shallow(<AddTodo />);
     expect(component.exists()).toEqual(true);
   });
 
   it('Should have one input', () => {
-    const component = shallow(<AddTodo />);
     expect(component.find('.todo-input').length).toEqual(1);
   });
 
-  it('Should have submit button', () => {
-    const component = shallow(<AddTodo />);
-    expect(component.find('.todo-submit').length).toEqual(1);
-  });
+  describe('Add todo button', () => {
+    it('Should exist', () => {
+      expect(component.find('.todo-submit').length).toEqual(1);
+    });
 
-  it('Should call the submitTodo function when clicked', () => {
-     const submitMock = jest.fn();
-     const component = mount(<AddTodo submitTodo={submitMock} />);
+    it('Should call the submitTodo function when clicked', () => {
+      component = mount(<AddTodo submitTodo={submitMock} />);
 
-     expect(submitMock.mock.calls.length).toEqual(0);
-     component.find('form').simulate('submit');
-     expect(submitMock.mock.calls.length).toEqual(1);
+      expect(submitMock.mock.calls.length).toEqual(0);
+      component.find('form').simulate('submit');
+      expect(submitMock.mock.calls.length).toEqual(1);
+    });
   });
 
 });
